694aac57dfbd1692c7a69c4e8b37dfb379236a3b
Nhan Tran <nhan.tran@eke.fi>
2022-07-05 00:09:32 +0300

    [client] Added draft version of Nav bar
    Nav bar currently has routing working

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Booking from './pages/booking';
+import AnnualReport from './pages/annual';
+import Teams from './pages/team';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/booking' element={<Booking/>} />
+        <Route path='/annual' element={<AnnualReport/>} />
+        <Route path='/team' element={<Teams/>} />
+        <Route path='/blogs' element={<Blogs/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/pages/booking.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Booking = () => {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'Right',
+                alignItems: 'Right',
+                height: '100vh'
+            }}
+        >
+            <h1>Welcome to Booking Calendar</h1>
+        </div>
+    );
+};
+
+export default Booking;
