ba62d51294fe7e9b95d0406149ac90d433160f44
nhan <tnnhan93@gmail.com>
2022-09-10 22:20:17 +0300

    [client] Booking page: used reducer for timetable week days.
    Changed from using regular state to reducer for timetable week days.

--- client/src/pages/booking.js
+++ client/src/pages/booking.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import "./styles/Booking.css"
 import Day from "../components/Day"
 import TimeFrameColumn from "../components/TimeFrameColumn"
@@ -7,25 +7,46 @@ import TechnicianSelector from "../components/TechnicianSelector";
 export default function Booking() {
     const today = new Date();
     const monDate = today.getDate() - today.getDay() + 1;
-    const [butt, setButt] = useState(0);
     const [records, setRecords] = useState([]);
-    const [weekDays, setWeekDays] = useState([{weekDay: "Mon", weekDate: new Date(today.setDate(monDate))},
-                                              {weekDay: "Tue", weekDate: new Date(today.setDate(monDate + 1))},
-                                              {weekDay: "Wed", weekDate: new Date(today.setDate(monDate + 2))},
-                                              {weekDay: "Thu", weekDate: new Date(today.setDate(monDate + 3))},
-                                              {weekDay: "Fri", weekDate: new Date(today.setDate(monDate + 4))},
-                                              {weekDay: "Sat", weekDate: new Date(today.setDate(monDate + 5))},
-                                              {weekDay: "Sun", weekDate: new Date(today.setDate(monDate + 6))},
-                                            ]);
+    const initialTimetableDays = [{weekDay: "Mon", weekDate: new Date(today.setDate(monDate))},
+                         {weekDay: "Tue", weekDate: new Date(today.setDate(monDate + 1))},
+                         {weekDay: "Wed", weekDate: new Date(today.setDate(monDate + 2))},
+                         {weekDay: "Thu", weekDate: new Date(today.setDate(monDate + 3))},
+                         {weekDay: "Fri", weekDate: new Date(today.setDate(monDate + 4))},
+                         {weekDay: "Sat", weekDate: new Date(today.setDate(monDate + 5))},
+                         {weekDay: "Sun", weekDate: new Date(today.setDate(monDate + 6))},]
     
+    function timetableDayReducer(state, action) {
+        switch (action.type) {
+            case "toNextWeek":
+                return state.map(day => (
+                    {
+                        ...day,
+                        weekDate: new Date(day.weekDate.setDate(day.weekDate.getDate() + 7))
+                    }
+                ))
+            case "toPrevWeek":
+                return state.map(day => (
+                    {
+                        ...day,
+                        weekDate: new Date(day.weekDate.setDate(day.weekDate.getDate() - 7))
+                    }
+                ))
+            default:
+                throw new Error();
+        }
+    }
+
+    const [timetableDays, dispatch] = useReducer(timetableDayReducer, initialTimetableDays);
+
     useEffect(() => {
         async function getRecords() {
-            let startTime = new Date(weekDays[0].weekDate);
+            let startTime = new Date(timetableDays[0].weekDate);
             startTime.setUTCHours(0);
             startTime.setUTCMinutes(0);
             startTime.setUTCSeconds(0);
             startTime.setUTCMilliseconds(0);
-            let endTime = new Date(weekDays[6].weekDate);
+            let endTime = new Date(timetableDays[6].weekDate);
             endTime.setUTCHours(23);
             endTime.setUTCMinutes(59);
             endTime.setUTCSeconds(59);
@@ -42,39 +63,17 @@ export default function Booking() {
             setRecords(recordResponse);
         }
         getRecords();
-        console.log("requested server")
-    }, [butt]);
+    }, [timetableDays]);
 
-    function toPrevWeek() {
-        setWeekDays(prevDays => prevDays.map(day => {
-            return {
-                ...day,
-                weekDate: new Date(day.weekDate.setDate(day.weekDate.getDate() - 7))
-            }
-        })
-        )
-        setButt(prev => prev=prev - 1)
-    }
-
-    function toNextWeek() {
-        setWeekDays(prevDays => prevDays.map(day => {
-            return {
-                ...day,
-                weekDate: new Date(day.weekDate.setDate(day.weekDate.getDate() + 7))
-            }
-        })
-        )
-        setButt(prev => prev = prev + 1)
-    }
     function calculateWeekNumber() {
-        const currentDate = weekDays[0].weekDate
+        const currentDate = timetableDays[0].weekDate
         const startDate = new Date(currentDate.getFullYear(), 0, 1);
         const days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
         const weekNumber = Math.ceil((currentDate.getDay() + 1 + days) / 7);
         return weekNumber;
     };
 
-    const populatedDays = weekDays.map(item => {
+    const populatedDays = timetableDays.map(item => {
         let tempArray = [];
         for(let i = 0; i < records.length; i++) {
             let tempDay = new Date(records[i].date).getDay();
@@ -92,18 +91,18 @@ export default function Booking() {
             <div className="calendar-container">
                 <div className="nav-week-container">
                     <h2 className="nav-week-number">Week {calculateWeekNumber()} -&nbsp;</h2>
-                    <h2 className="nav-week-year">{weekDays[0].weekDate.getFullYear()}</h2>
+                    <h2 className="nav-week-year">{timetableDays[0].weekDate.getFullYear()}</h2>
                     <TechnicianSelector className="nav-week-technician-selector"/>
                 </div>
                 <div className="timetable-container">
-                    <button className="prev-week-button" onClick={toPrevWeek}>Prev Week</button>
+                    <button className="prev-week-button" onClick={() => dispatch({type: "toPrevWeek"})}>Prev Week</button>
                     <div className="time-frame-column">
                         <TimeFrameColumn openTime={8} closeTime={17} slotTime={0.5} />
                     </div>
                     <div className="day-container">
                         {populatedDays}
                     </div>
-                    <button className="next-week-button" onClick={toNextWeek}>Next Week</button>
+                    <button className="next-week-button" onClick={() => dispatch({type: "toNextWeek"})}>Next Week</button>
                 </div>
             </div>
         </div>
